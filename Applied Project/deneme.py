155cfade38b6151b94fce41bdd622fa3b5a54b4e
U150709029 <selahattinaksoy@posta.mu.edu.tr>
2020-03-04 23:39:05 +0300

    last arrangment

--- Applied Project/deneme.py
+++ Applied Project/deneme.py
@@ -1,4 +1,6 @@
-import librosa
-audio_data = "C:\\Users\\selah\\Desktop\\Applied Machine Learning\\Data\\CF003 - Active - Day - (214).wav"
-x , sr = librosa.load(audio_data)
-print(type(x), type(sr))#<class 'numpy.ndarray'> <class 'int'>print(x.shape, sr)#(94316,) 22050
+from pydub import AudioSegment
+t1 = 36.34 * 1000 #Works in milliseconds
+t2 = 37.65 * 1000
+newAudio = AudioSegment.from_wav("C:\\Users\\selah\\Desktop\\Applied Machine Learning\\Data\\CF003 - Active - Day - (214).wav")
+newAudio = newAudio[t1:t2]
+newAudio.export('C:\\Users\\selah\\Desktop\\Applied Machine Learning\\mert.wav', format="wav") #Exports to a wav file in the current path.
