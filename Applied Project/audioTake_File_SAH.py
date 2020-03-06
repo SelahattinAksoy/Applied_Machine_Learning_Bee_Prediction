3916d98eb0b7ba72187f703ab8b9a59bd46f951b
Ali Emre Kürekci <aliemrekurekci@gmail.com>
2020-03-06 17:07:45 +0300

    zaman aralıkları listelendi

--- Applied Project/audioTake_File_SAH.py
+++ Applied Project/audioTake_File_SAH.py
@@ -1,30 +1,39 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import wave
-import sys
+from pydub import AudioSegment
 
+soundFile = input("Please enter sound file name: ")
+spf = wave.open("Data\\" + soundFile + ".wav", "r") #wave objesi
 
-spf = wave.open("C:\\Users\\selah\\Desktop\\Applied Machine Learning\\mert.wav", "r") #wave objesi
+labFile = open("Data\\" + soundFile + ".lab", "r")
+line = labFile.readline()
+line = labFile.readline()
+timeList = []
+while line:
+    line = line.split('	')
+    line.pop(-1)#to get rid of bee nobee
+    timeList.append(line)
+    line = labFile.readline()
 
 # Extract Raw Audio from Wav File
-signal = spf.readframes(-1)     #wave objesinşdeki frami oku
-signal = np.fromstring(signal, "Int16")
+signal = spf.readframes(-1)     #wave objesindeki frami oku
+signal = np.frombuffer(signal, dtype="Int16")
 
-fs = spf.getframerate()
-
-Time = np.linspace(0, len(signal) / fs, num=len(signal))
+frameRate = spf.getframerate()
+Time = np.linspace(0, len(signal) / frameRate, num=len(signal))
 
 print(spf.getnframes())  #frame aldık  par part
 print(spf.getframerate())  #frekans aldık
 print(spf.getsampwidth())
 print(spf.getnchannels())
-print (len(signal))
-print (signal)
-a=signal[1:100][1:100]
+print(len(signal))
+print(signal)
+a = signal[1:100][1:100]
 
 plt.figure(1)
 plt.title("Signal Wave...")
-plt.plot(Time,signal)  #,color="yellow",linestyle="dashed"
-plt.savefig("C:\\Users\\selah\\Desktop\\Applied Machine Learning\\Applied_Machine_Learning_Bee_Prediction\\aliemre2.png")
+plt.plot(Time, signal)  #,color="yellow",linestyle="dashed"
+plt.savefig("pic.png")
 plt.show()
 
