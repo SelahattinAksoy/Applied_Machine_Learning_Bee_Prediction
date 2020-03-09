ba738929b2ac46c0e3668bd5ca5749fbe55e9450
U150709029 <selahattinaksoy@posta.mu.edu.tr>
2020-03-09 19:25:29 +0300

    kaarşim

--- Applied Project/audioTake_File_SAH.py
+++ Applied Project/audioTake_File_SAH.py
@@ -3,36 +3,58 @@ import numpy as np
 import wave
 from pydub import AudioSegment
 
-soundFile = input("Please enter sound file name: ")
-spf = wave.open("Data\\" + soundFile + ".wav", "r") #wave objesi
 
-labFile = open("Data\\" + soundFile + ".lab", "r")
-line = labFile.readline()
-line = labFile.readline()
-timeList = []
-while line:
-    line = line.split('	')
-    if line[0] == '.\n':
-        break
-    eleman = float(line.pop(0))
-    line.insert(0, eleman)
-    eleman2 = float(line.pop(1))
-    line.insert(1, eleman2)
-    #line.pop(-1)#to get rid of bee nobee
-    timeList.append(line)
-    line = labFile.readline()
-print(timeList)
-newAudio = AudioSegment.from_wav("Data\\" + soundFile + ".wav")
-partOfNewAudio = newAudio[timeList[0][0]*1000:timeList[0][1]*1000]
-partOfNewAudio.export("SoundParts\\part1.wav", format="wav")
-spf = wave.open("SoundParts\\part1.wav", "r")
-# Extract Raw Audio from Wav File
-signal = spf.readframes(-1)     #wave objesindeki frami oku
-signal = np.frombuffer(signal, dtype="Int16")
+list_of_sound_file_name=[]
+for i in range(214,227,1):
+    sound=str(i)
+    sound="("+sound+")"
+    soundFile="CF003 - Active - Day - "+sound
+    list_of_sound_file_name.append(soundFile)
+print(list_of_sound_file_name)
 
-frameRate = spf.getframerate()
 
-Time = np.linspace(0, len(signal) / frameRate, num=len(signal))
+for j in list_of_sound_file_name:
+
+        spf = wave.open("Data\\" + j + ".wav", "r") #wave objesi
+
+        labFile = open("Data\\" + j + ".lab", "r")
+
+
+        line = labFile.readline()
+        line = labFile.readline()
+        timeList = []
+        while line:
+            line = line.split('	')
+            if line[0] == '.\n':
+                break
+            eleman = float(line.pop(0))
+            line.insert(0, eleman)
+            eleman2 = float(line.pop(1))
+            line.insert(1, eleman2)
+            #line.pop(-1)#to get rid of bee nobee
+            timeList.append(line)
+            line = labFile.readline()
+        print(timeList)
+
+        counter=0;
+        for i in timeList:
+            newAudio = AudioSegment.from_wav("Data\\" + j + ".wav")
+            partOfNewAudio = newAudio[i[0] * 1000:i[1] * 1000]
+            partOfNewAudio.export("Cleaned Data\\Cleaned_Voice\\"+j+"--"+(i[2])[:-2]+"--"+str(counter)+".wav", format="wav")
+
+
+            spf = wave.open("Cleaned Data\\Cleaned_Voice\\"+j+"--"+(i[2])[:-2]+"--"+str(counter)+".wav", "r")
+            # Extract Raw Audio from Wav File
+            signal = spf.readframes(-1)     #wave objesindeki frami oku
+            signal = np.frombuffer(signal, dtype="Int16")
+            frameRate = spf.getframerate()
+            Time = np.linspace(0, len(signal) / frameRate, num=len(signal))
+            plt.figure(1)
+            plt.title(j+"--"+(i[2])[:-2]+"--"+str(counter))
+            plt.plot(Time, signal)  # ,color="yellow",linestyle="dashed"
+            plt.savefig("Cleaned Data\\Freq_of_Cleaned_Voice\\"+j+"--"+(i[2])[:-2]+"--"+str(counter)+".png")
+            counter += 1
+
 
 print(spf.getnframes())  #frame aldık  par part
 print(spf.getframerate())  #frekans aldık
@@ -41,9 +63,6 @@ print(spf.getnchannels())
 print(len(signal))
 print(signal)
 
-plt.figure(1)
-plt.title("Signal Wave...")
-plt.plot(Time, signal)  #,color="yellow",linestyle="dashed"
-plt.savefig("pic.png")
-plt.show()
+
+#plt.show()
 
